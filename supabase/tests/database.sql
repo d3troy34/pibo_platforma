edaa200c76d23fc1160653d103d1eea324cc5ea4
d3troy34 <francobalestieri13@gmail.com>
2026-07-27 18:34:10 -0300

    Correct database test plan count

--- supabase/tests/database.sql
+++ supabase/tests/database.sql
@@ -2,7 +2,7 @@ begin;
 
 create extension if not exists pgtap with schema extensions;
 
-select plan(131);
+select plan(130);
 
 select has_table('public', 'profiles', 'profiles table exists');
 select has_table('public', 'profile_directory', 'chat-safe profile directory exists');
